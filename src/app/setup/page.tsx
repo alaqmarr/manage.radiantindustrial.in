33f5400270e0fa142fdef7c685d1bb2728ae892d
Al Aqmar <alaqmarak0810@gmail.com>
2026-08-05 21:52:47 +0530

    dep

--- src/app/setup/page.tsx
+++ src/app/setup/page.tsx
@@ -2,6 +2,8 @@ import { redirect } from "next/navigation"
 import { prisma } from "@/lib/prisma"
 import { SetupForm } from "./SetupForm"
 
+export const dynamic = "force-dynamic"
+
 export default async function SetupPage() {
   const adminCount = await prisma.user.count({
     where: { role: "ADMIN" }
